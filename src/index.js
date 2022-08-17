cad8fd7d425ee1ed907c3377f5689fb934094b2f
Tommy Egbe <tabe_e@yahoo.ca>
2022-08-17 14:58:21 +0200

    added atatus libary and coe to do app performance

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('70a8b897e8a4453d97c96ae0412e7839').install();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +18,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
