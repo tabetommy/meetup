b5d6a17df82884ab69bb493d45697fd08038e724
Tommy Egbe <tabe_e@yahoo.ca>
2022-08-21 01:36:30 +0200

    added service worker and customised manifest.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 atatus.config('70a8b897e8a4453d97c96ae0412e7839').install();
 
@@ -17,6 +19,7 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+serviceWorkerRegistration.register()
 
 
