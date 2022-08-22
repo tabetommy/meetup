1b3a298c1b4a08a98e12f1ba9c367d22f64d1671
Tommy Egbe <tabe_e@yahoo.ca>
2022-08-23 01:54:08 +0200

    added scatter plot to show number of events by city

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations,getEvents } from '../api';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip,ResponsiveContainer
+} from 'recharts';
 
 class App extends Component {
   constructor() {
@@ -45,6 +48,16 @@ class App extends Component {
       });
     }
 
+    getData = () => {
+      const {locations, events} = this.state;
+      const data = locations.map((location)=>{
+        const number = events.filter((event) => event.location === location).length
+        const city = location.split(', ').shift()
+        return {city, number};
+      })
+      return data;
+    };
+
     componentWillUnmount(){
       this.mounted = false;
     }
@@ -60,6 +73,16 @@ class App extends Component {
         eventsCount={this.state.eventsCount}
         updateEvents={this.updateEvents}
         />
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="category" dataKey="city" name="city"/>
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false}/>
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events}/>
       </div>
     );
